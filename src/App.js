32818de3c1d09d1afae8b7ddfd9532e8fc57e939
Aflah Kaunyn <aflahkaunyn@gmail.com>
2025-08-17 14:44:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import TaskList from './components/TaskList';
+import ProtectedPage from './components/ProtectedPage';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem('token'));
@@ -12,20 +15,32 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Task Manager</h1>
-      {!loggedIn ? (
-        <>
-          <Signup />
-          <Login onLogin={() => setLoggedIn(true)} />
-        </>
-      ) : (
-        <>
-          <button onClick={handleLogout}>Logout</button>
-          <TaskList />
-        </>
-      )}
-    </div>
+    <Router>
+      <div>
+        <h1>Task Manager</h1>
+        <button onClick={handleLogout}>Logout</button>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login onLogin={() => setLoggedIn(true)} />} />
+          <Route
+            path="/tasks"
+            element={
+              <PrivateRoute>
+                <TaskList />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/protected"
+            element={
+              <PrivateRoute>
+                <ProtectedPage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
